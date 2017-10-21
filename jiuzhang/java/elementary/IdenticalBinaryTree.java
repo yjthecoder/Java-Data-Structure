12acff3048fbfed731314800ce96c09121d665c7
Yuan Ji <yj.the.coder@gmail.com>
2017-10-21 17:19:16 -0400

    Add more tree problems

--- jiuzhang/java/elementary/IdenticalBinaryTree.java
+++ jiuzhang/java/elementary/IdenticalBinaryTree.java
@@ -1,5 +1,20 @@
 package jiuzhang.java.elementary;
 
+//Identical Binary Tree
 public class IdenticalBinaryTree {
-
+    public boolean isIdentical(TreeNode a, TreeNode b) {
+        // write your code here
+        if (a == null) {
+            return b == null;
+        }
+        
+        if (b == null) {
+            return a == null;
+        }
+        
+        //write the regular cases first and consider the exit of recursion both logically
+        // and gramatically  , then write them
+        return a.val == b.val && isIdentical(a.left, b.left) 
+                        &&  isIdentical(a.right, b.right);
+    }
 }
---
+++ jiuzhang/java/elementary/TreeNode.java
@@ -0,0 +1,12 @@
+package jiuzhang.java.elementary;
+
+public class TreeNode {
+    public int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int val_){
+        val = val_;
+        left = null;
+        right = null;
+    }
+}
